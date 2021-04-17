340b9fca8ffbacc289c9ea8953ab8eb580acf23f
ledrsnet <31727281+ledrsnet@users.noreply.github.com>
2021-04-17 19:48:52 +0800

    提示服务成功或失败的脚本函数
    函数名：getOkFailed
    入参：
           $1 = 打印的字符
           $2 = ok|failed

---
+++ printok.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# 
+#####################################################################
+#
+#Author:                LiangDong
+#Email:                 395539184@qq.com
+#Date:                  2021-04-17
+#FileName：             printok.sh
+#URL:                   https://github.com/ledrsnet
+#Description：          
+#Copyright (C):         2021 All rights reserved
+#
+#####################################################################
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+END="\e[0m"
+WEIGHT=100
+ALIGN="-" # - align left; "" align right
+
+
+getOkFailed(){
+local tempvar=`echo $2|tr 'A-Z' 'a-z'`
+local length=${#1}
+case $tempvar in
+ok)
+    if [ $length -ge 100 ]; then
+        echo "$1"
+        printf "%${ALIGN}${WEIGHT}s" && printf "[$GREEN  OK  $END]"
+    else
+        printf "%${ALIGN}${WEIGHT}s" "$1" && printf "[$GREEN  OK  $END]"
+    fi
+    echo
+    ;;
+
+failed)
+    if [ $length -ge 100 ]; then
+        echo "$1"
+        printf "%${ALIGN}${WEIGHT}s" && printf "[${RED}FAILED$END]"
+    else
+        printf "%${ALIGN}${WEIGHT}s" "$1" && printf "[${RED}FAILED$END]"
+    fi
+    echo
+    ;;
+*)
+    echo "Usage: `basename $0` ok|failed,return string with color."
+    ;;
+esac
+}
