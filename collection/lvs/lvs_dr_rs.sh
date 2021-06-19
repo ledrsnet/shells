ddcb7fd8f0c4be381f4a04b8a1b4baef9da2fe89
root <root@maple-u18.maple.com>
2021-06-19 10:54:14 +0800

    upload lvs shells

---
+++ collection/lvs/lvs_dr_rs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Author:wangxiaochun
+#Date:2017-08-13
+vip=172.16.0.100
+mask='255.255.255.255'
+dev=lo:1
+#rpm -q httpd &> /dev/null || yum -y install httpd &>/dev/null
+#service httpd start &> /dev/null && echo "The httpd Server is Ready!"
+#echo "`hostname -I`" > /var/www/html/index.html
+
+case $1 in
+start)
+    echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore
+    echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore
+    echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
+    echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce
+    ifconfig $dev $vip netmask $mask #broadcast $vip up
+    #route add -host $vip dev $dev
+    echo "The RS Server is Ready!"
+    ;;
+stop)
+    ifconfig $dev down
+    echo 0 > /proc/sys/net/ipv4/conf/all/arp_ignore
+    echo 0 > /proc/sys/net/ipv4/conf/lo/arp_ignore
+    echo 0 > /proc/sys/net/ipv4/conf/all/arp_announce
+    echo 0 > /proc/sys/net/ipv4/conf/lo/arp_announce
+    echo "The RS Server is Canceled!"
+    ;;
+*) 
+    echo "Usage: $(basename $0) start|stop"
+    exit 1
+    ;;
+esac
